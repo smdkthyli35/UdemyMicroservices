7c5bc95f02439a1cd990834303c82c503be6a989
zeljkosamed <smdkthyli@gmail.com>
2022-07-11 20:50:12 +0300

    CatalogApi - GeneralMapping added.

---
+++ src/Services/CatalogService/CatalogService.Api/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CatalogService.Api.Dtos;
+using CatalogService.Api.Models;
+
+namespace CatalogService.Api.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>()
+                .ReverseMap();
+            CreateMap<Category, CategoryDto>()
+                .ReverseMap();
+            CreateMap<Feature, FeatureDto>()
+                .ReverseMap();
+            CreateMap<Course, CourseCreateDto>()
+                .ReverseMap();
+            CreateMap<Course, CourseUpdateDto>()
+                .ReverseMap();
+        }
+    }
+}
