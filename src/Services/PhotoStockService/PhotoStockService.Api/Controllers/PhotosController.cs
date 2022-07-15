301438cbc0d22a536ea08cbb6586158ecb32664a
zeljkosamed <smdkthyli@gmail.com>
2022-07-15 21:28:08 +0300

    PhotoStockService - PhotosController PhotoDelele endpoint added.

--- src/Services/PhotoStockService/PhotoStockService.Api/Controllers/PhotosController.cs
+++ src/Services/PhotoStockService/PhotoStockService.Api/Controllers/PhotosController.cs
@@ -32,5 +32,19 @@ namespace PhotoStockService.Api.Controllers
 
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty.", 400));
         }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found!", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
     }
 }
