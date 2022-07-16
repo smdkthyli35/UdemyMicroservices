681122f8d118095a13fb9542276e469cc55471b3
zeljkosamed <smdkthyli@gmail.com>
2022-07-16 18:03:34 +0300

    BasketService - IBasketService and RedisService added.

---
+++ src/Services/BasketService/BasketService.Api/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using BasketService.Api.Dtos;
+using UdemyMicroservices.Common.Dtos;
+
+namespace BasketService.Api.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
