10a871058281fc08fbd49395d39ad76695f30011
zeljkosamed <smdkthyli@gmail.com>
2022-07-16 17:44:25 +0300

    BasketService - BasketDto and BasketItemDto added.

---
+++ src/Services/BasketService/BasketService.Api/Dtos/BasketDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace BasketService.Api.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> basketItems { get; set; }
+        public decimal TotalPrice
+        {
+            get => basketItems.Sum(x => x.Price * x.Quantity);
+        }
+    }
+}
---
+++ src/Services/BasketService/BasketService.Api/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BasketService.Api.Dtos
+{
+    public class BasketItemDto
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
