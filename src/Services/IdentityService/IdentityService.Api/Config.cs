a4c9909343b833593db02e49a2bd4dcefd1b936b
zeljkosamed <smdkthyli@gmail.com>
2022-07-19 16:55:05 +0300

    DiscountService - Identity config and program.cs updated.

--- src/Services/IdentityService/IdentityService.Api/Config.cs
+++ src/Services/IdentityService/IdentityService.Api/Config.cs
@@ -14,6 +14,7 @@ namespace IdentityService.Api
             new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
             new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+            new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -38,6 +39,7 @@ namespace IdentityService.Api
                 new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
                 new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                 new ApiScope("basket_fullpermission","Basket API için full erişim"),
+                new ApiScope("discount_fullpermission","Discount API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -63,6 +65,7 @@ namespace IdentityService.Api
                     AllowedScopes =
                     {
                         "basket_fullpermission",
+                        "discount_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
