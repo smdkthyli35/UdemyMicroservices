2051357b014a0ea447cf5ee40726b30edf177936
zeljkosamed <smdkthyli@gmail.com>
2022-07-20 16:42:07 +0300

    OrderService - CreateOrder command and handler added.

---
+++ src/Services/OrderService/OrderService.Application/Features/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using OrderService.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyMicroservices.Common.Dtos;
+
+namespace OrderService.Application.Features.Commands.CreateOrder
+{
+    public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto AddressDto { get; set; }
+    }
+}
---
+++ src/Services/OrderService/OrderService.Application/Features/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using OrderService.Application.Dtos;
+using OrderService.Domain.OrderAggregate;
+using OrderService.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyMicroservices.Common.Dtos;
+
+namespace OrderService.Application.Features.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+
+        public CreateOrderCommandHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(request.AddressDto.Province, request.AddressDto.District, request.AddressDto.Street, request.AddressDto.ZipCode, request.AddressDto.Line);
+            Order newOrder = new Order(newAddress, request.BuyerId);
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await _context.Orders.AddAsync(newOrder);
+            await _context.SaveChangesAsync();
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id }, 200);
+        }
+    }
+}
