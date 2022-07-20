99cdecd9382f3059d359c5d50e179ace6a5173fd
zeljkosamed <smdkthyli@gmail.com>
2022-07-20 16:10:37 +0300

    OrderService - CustomMapping and ObjectMapper added.

---
+++ src/Services/OrderService/OrderService.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using OrderService.Application.Dtos;
+using OrderService.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
