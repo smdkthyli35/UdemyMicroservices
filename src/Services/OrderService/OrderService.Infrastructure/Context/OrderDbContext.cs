6d6d7e6fa213db978a9426e2dae27cf6376c6eab
zeljkosamed <smdkthyli@gmail.com>
2022-07-20 15:42:10 +0300

    OrderService - OrderDbContext created.

---
+++ src/Services/OrderService/OrderService.Infrastructure/Context/OrderDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Context
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItem", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<OrderItem>().Property(i => i.Price).HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<Order>().OwnsOne(i => i.Address).WithOwner();
+        }
+    }
+}
