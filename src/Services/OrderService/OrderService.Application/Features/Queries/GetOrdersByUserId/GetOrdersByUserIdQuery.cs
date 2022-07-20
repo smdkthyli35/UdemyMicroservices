99a612152d9c8486e30ac67a1c4c36dbaabb6059
zeljkosamed <smdkthyli@gmail.com>
2022-07-20 16:26:20 +0300

    OrderService - GetOrdersByUserId query and handler added.

---
+++ src/Services/OrderService/OrderService.Application/Features/Queries/GetOrdersByUserId/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using OrderService.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyMicroservices.Common.Dtos;
+
+namespace OrderService.Application.Features.Queries.GetOrdersByUserId
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
