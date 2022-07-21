c057d4e21c15e154f6b5ec8f14044a7d4b877ae7
zeljkosamed <smdkthyli@gmail.com>
2022-07-21 14:43:31 +0300

    OrderService - Config implementation added.

--- src/Services/IdentityService/IdentityService.Api/Config.cs
+++ src/Services/IdentityService/IdentityService.Api/Config.cs
@@ -15,6 +15,7 @@ namespace IdentityService.Api
             new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -40,6 +41,7 @@ namespace IdentityService.Api
                 new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                 new ApiScope("basket_fullpermission","Basket API için full erişim"),
                 new ApiScope("discount_fullpermission","Discount API için full erişim"),
+                new ApiScope("order_fullpermission","Order API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -66,6 +68,7 @@ namespace IdentityService.Api
                     {
                         "basket_fullpermission",
                         "discount_fullpermission",
+                        "order_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
--- src/Services/OrderService/OrderService.Api/Program.cs
+++ src/Services/OrderService/OrderService.Api/Program.cs
@@ -1,7 +1,11 @@
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using OrderService.Application.Features.Commands.CreateOrder;
 using OrderService.Infrastructure.Context;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 using UdemyMicroservices.Common.Services;
 
@@ -17,13 +21,26 @@ builder.Services.AddDbContext<OrderDbContext>(options =>
     });
 });
 
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_order";
+    options.RequireHttpsMetadata = false;
+});
+
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<ICommonIdentityService, CommonIdentityService>();
 
 builder.Services.AddMediatR(typeof(CreateOrderCommandHandler).Assembly);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
