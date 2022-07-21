b90d69f9a01e2d1a89a59911b81b1cf596a9d77a
zeljkosamed <smdkthyli@gmail.com>
2022-07-21 15:33:02 +0300

    FakePaymentService - FakePaymentsController added.

---
+++ src/Services/FakePaymentService/FakePaymentService.Api/Controllers/FakePaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyMicroservices.Common.ControllerBases;
+using UdemyMicroservices.Common.Dtos;
+
+namespace FakePaymentService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
