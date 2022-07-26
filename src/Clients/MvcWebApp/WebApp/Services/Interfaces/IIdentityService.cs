70a85792081577f70069865df3c4af6286c1db84
zeljkosamed <smdkthyli@gmail.com>
2022-07-26 16:29:48 +0300

    Mvc - IIdentityService and IdentityModel library added.

---
+++ src/Clients/MvcWebApp/WebApp/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel.Client;
+using UdemyMicroservices.Common.Dtos;
+using WebApp.Models;
+
+namespace WebApp.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
