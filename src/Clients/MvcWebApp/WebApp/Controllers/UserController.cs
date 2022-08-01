c458f4e86be6065e4924dfbc2af0a0102de254f4
zeljkosamed <smdkthyli@gmail.com>
2022-08-01 16:24:47 +0300

    Mvc - UserController/Index.cshtml added.

---
+++ src/Clients/MvcWebApp/WebApp/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Services.Interfaces;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userService.GetUser());
+        }
+    }
+}
--- src/Clients/MvcWebApp/WebApp/Program.cs
+++ src/Clients/MvcWebApp/WebApp/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddHttpContextAccessor();
 
 var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
+builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
 
 builder.Services.AddHttpClient<IUserService, UserService>(opt =>
